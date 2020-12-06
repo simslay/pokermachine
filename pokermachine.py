9327313a410381ab530915dd0188c93e9b4257fd
simslay <simon.grandsire@gmail.com>
2020-12-06 20:54:50 +0100

    Build game

--- pokermachine.py
+++ pokermachine.py
@@ -5,6 +5,54 @@ from tkinter import *
 
 
 def main():
+    class Card(object):
+        def __init__(self, value, suit):
+            self.value = value
+            self.suit = suit
+            self.showing = True
+
+        def __repr__(self):
+            value_name = ""
+            suit_name = ""
+            if self.showing:
+                if self.value == 0:
+                    value_name = "Two"
+                if self.value == 1:
+                    value_name = "Three"
+                if self.value == 2:
+                    value_name = "Four"
+                if self.value == 3:
+                    value_name = "Five"
+                if self.value == 4:
+                    value_name = "Six"
+                if self.value == 5:
+                    value_name = "Seven"
+                if self.value == 6:
+                    value_name = "Eight"
+                if self.value == 7:
+                    value_name = "Nine"
+                if self.value == 8:
+                    value_name = "Ten"
+                if self.value == 9:
+                    value_name = "Jack"
+                if self.value == 10:
+                    value_name = "Queen"
+                if self.value == 11:
+                    value_name = "King"
+                if self.value == 12:
+                    value_name = "Ace"
+                if self.suit == 0:
+                    suit_name = "Diamonds"
+                if self.suit == 1:
+                    suit_name = "Clubs"
+                if self.suit == 2:
+                    suit_name = "Hearts"
+                if self.suit == 3:
+                    suit_name = "Spades"
+                return value_name + " of " + suit_name
+            else:
+                return "[CARD]"
+
     class StandardDeck(list):
         def __init__(self):
             super().__init__()
@@ -33,10 +81,46 @@ def main():
 
     def play(game):
         game.deck.shuffle()
+        game_info_q.put(game)
+        # update_gui(game)
+        game.establish_player_attributes()
+        game.deal_hole()
+        game.print_round_info()
+        game.act_one()
+        game.print_round_info()
+        if not game.round_ended:
+            game.deal_flop()
+            game.print_round_info()
+        if not game.round_ended:
+            game.ask_players()
+            game.print_round_info()
+        if not game.round_ended:
+            game.deal_turn()
+            game.print_round_info()
+        if not game.round_ended:
+            game.ask_players()
+            game.print_round_info()
+        if not game.round_ended:
+            game.deal_river()
+            game.print_round_info()
+        if not game.round_ended:
+            game.ask_players()
+            game.print_round_info()
+        if not game.round_ended:
+            game.score_all()
+            game.print_round_info()
+        game.find_winners()
+        game_info_q.put(game)
+
+        game.print_round_info()
+        game.round_ended = True
+        print(game.winners, game.winner, [player for player in game.list_of_players_not_out if player.win])
+        game.end_round()
 
     class App(Tk):
         def __init__(self, *args, **kwargs):
             Tk.__init__(self, *args, **kwargs)
+            self.game_object = object
 
     def run_app():
         app = App()
@@ -51,9 +135,9 @@ def main():
     response_q = queue.Queue()
     game_info_q = queue.Queue()
     end_update = threading.Event()
-    t1 = threading.Thread(target=run_app)
-    t1.start()
-    t2 = threading.Thread(target=run_game_data())
+    # t1 = threading.Thread(target=run_app)
+    # t1.start()
+    t2 = threading.Thread(target=run_game_data)
     t2.start()
 
 
