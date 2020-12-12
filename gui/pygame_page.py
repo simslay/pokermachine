8d29f85a9b3847bc8bb25b7fb40c9364116575c4
simslay <simon.grandsire@gmail.com>
2020-12-12 13:22:13 +0100

    Build game

--- gui/pygame_page.py
+++ gui/pygame_page.py
@@ -18,24 +18,28 @@ class PygamePage:
 
         screen = pygame.display.set_mode((window_width, window_height))
 
-        south_cards = pygame.image.load('images/600px-2Cards_South.svg.png')
-        north_cards = pygame.image.load('images/600px-2Cards_North.svg.png')
-        south_east_cards = pygame.image.load('images/600px-2Cards_South-East.svg.png')
-        south_west_cards = pygame.image.load('images/600px-2Cards_South-West.svg.png')
-        north_east_cards = pygame.image.load('images/600px-2Cards_North-East.svg.png')
-        north_west_cards = pygame.image.load('images/600px-2Cards_North-West.svg.png')
+        south_table_cards = pygame.image.load('images/600px-2Cards_South.svg.png')
+        north_table_cards = pygame.image.load('images/600px-2Cards_North.svg.png')
+        south_table_east_cards = pygame.image.load('images/600px-2Cards_South-East.svg.png')
+        south_table_west_cards = pygame.image.load('images/600px-2Cards_South-West.svg.png')
+        north_table_east_cards = pygame.image.load('images/600px-2Cards_North-East.svg.png')
+        north_table_west_cards = pygame.image.load('images/600px-2Cards_North-West.svg.png')
+
+        south_card1 = pygame.image.load('images/cards/Ace of Clubs.png')
+        south_card1 = pygame.transform.scale(south_card1, (691//10, 1056//10))
 
         end = False
 
         while not end:
             screen.fill((220, 220, 220))
             screen.blit(table_img, table_rect)
-            screen.blit(south_cards, (window_width//2-53//2, window_height//2+32))
-            screen.blit(north_cards, (window_width//2-53//2, 117))
-            screen.blit(south_east_cards, (window_width-217, window_height//2-6))
-            screen.blit(south_west_cards, (163, window_height//2-6))
-            screen.blit(north_east_cards, (window_width-217, 144))
-            screen.blit(north_west_cards, (166, 144))
+            screen.blit(south_table_cards, (window_width//2-53//2, window_height//2+32))
+            screen.blit(south_card1, (window_width//2-53//2, window_height//2+32+70))
+            screen.blit(north_table_cards, (window_width//2-53//2, 117))
+            screen.blit(south_table_east_cards, (window_width-217, window_height//2-6))
+            screen.blit(south_table_west_cards, (163, window_height//2-6))
+            screen.blit(north_table_east_cards, (window_width-217, 144))
+            screen.blit(north_table_west_cards, (166, 144))
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     end = True
